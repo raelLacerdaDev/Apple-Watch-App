19f3f36491175f267959e5609bac3bd8e6873135
anaclaracald <ana05clara24@gmail.com>
2026-08-14 17:10:39 -0300

    chore: creating watch os project

---
+++ Apresentation/Apresentation Watch App/ApresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApresentationApp.swift
+//  Apresentation Watch App
+//
+//  Created by Ana Clara Ferreira Caldeira on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Apresentation_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
