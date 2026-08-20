cd66bfc01e28d8e9efe5c10938cbb3228f498cde
anaclaracald <ana05clara24@gmail.com>
2026-08-20 17:06:37 -0300

    TRY: solving conflicts

---
+++ Apresentation/Apresentation Watch App/Presentation/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Apresentation Watch App
+//
+//  Created by Ana Clara Ferreira Caldeira on 14/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@EnvironmentObject var workoutManager: WorkoutManager
+	
+	var body: some View {
+		VStack {
+			Text("Hello word")
+		}
+		.padding()
+		.task {
+			await workoutManager.requestAuthorization()
+        }
+	}
+}
+
+#Preview {
+    ContentView()
+}
